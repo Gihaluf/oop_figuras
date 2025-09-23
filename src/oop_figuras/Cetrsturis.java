c9b70dc0f93e86ddeed735a63208fa17b5a96c39
Gihaluf <gihaluf.bojar@gmail.com>
2025-09-23 11:35:32 +0300

    Pabegta klase Cetrsturis

--- src/oop_figuras/Cetrsturis.java
+++ src/oop_figuras/Cetrsturis.java
@@ -1,5 +1,26 @@
 package oop_figuras;
 
 public class Cetrsturis {
-
+	public double platums, augstums;
+	public Centrs punkts;
+	
+	// KOnstruktora pārslogošana
+	public Cetrsturis() {
+		punkts = new Centrs (0, 0);
+	}
+	
+	public Cetrsturis(Centrs punkts, double p, double a) {
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+	}
+	
+	public String izvaditCetrsturaInfo() {
+		return "Platums: "+platums+"cm\nAugstums: "+augstums+"cm";
+	}
+	//Uztaisīt metodi, kas aprēķina laukumu
+	
+	public double cLaukums() {
+		return platums*augstums;
+	}
 }
