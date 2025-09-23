1f3ee28bc3a2d2bda550d83d978ad54bfdd22cbc
Gihaluf <gihaluf.bojar@gmail.com>
2025-09-23 11:47:20 +0300

    Pabeigta klase Aplis

--- src/oop_figuras/Aplis.java
+++ src/oop_figuras/Aplis.java
@@ -1,5 +1,28 @@
 package oop_figuras;
 
 public class Aplis {
-
+	public Centrs punkts;
+	public double radiuss;
+	
+	public Aplis(Centrs p, double r) {
+		punkts = p;
+		radiuss = r;
+	}
+	
+	public String izvaditAplaInfo() {
+		return "Radiuss: "+radiuss;
+	}
+	
+	//izveidot metodi, riņķa līnijas aprēķināšanai
+	
+	public double rinkaLinija() {
+		double C = 2*Math.PI*radiuss;
+		return 	Math.round((C*100.0)/100.0);
+	}
+	
+	//rinķa laukuma aprēķināšana
+	public double rinkaLaukums() {
+		double S= Math.PI*Math.pow(radiuss, 2);
+		return Math.round((S*100.0)/100.0);
+	}
 }
